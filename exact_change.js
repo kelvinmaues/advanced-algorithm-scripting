86628d1bf2a1e013e2972aed7bcdcd93b733a81f
Kelvin <kelvinmaus@gmail.com>
2017-10-15 23:26:21 -0300

    Algoritmo solucionado!

--- exact_change.js
+++ exact_change.js
@@ -1,7 +1,8 @@
 function checkCashRegister(price, cash, cid) {
-  // Currency values
+  // Valor de troco
   var change = cash - price;
 
+  // Valor atual de moedas
   var denom = [
     { name: 'ONE HUNDRED', val: 100.00},
     { name: 'TWENTY', val: 20.00},
@@ -14,8 +15,10 @@ function checkCashRegister(price, cash, cid) {
     { name: 'PENNY', val: 0.01}
   ];
 
+  // Reverte a ordem dos valores de caixa repassados
   var cidReversed = cid.reverse();
 
+  //Função calcula os valores de caixa que podem ser repassados como troco
   function isBigEnough(value) {
     for(var i = 0; i < denom.length; i++){
       //console.log(denom[i])
@@ -24,9 +27,10 @@ function checkCashRegister(price, cash, cid) {
       }
     }
   }
+  // Filtra os valores que são menores que o valor de troco
   var filteredCID = cidReversed.filter(isBigEnough);
 
-  // know the value of each currency type in the register
+  // Verifica o valor padrão da moeda
   function checkDenom(denomName){
     for(var i = 0; i < denom.length; i++){
       if(denom[i].name === denomName) {
@@ -35,41 +39,26 @@ function checkCashRegister(price, cash, cid) {
     }
   }
 
-  /*
-  function calculateChange(){
-    var changeArr = [];
-    console.log(filteredCID)
-    for(var i = 0, count= 0; i < filteredCID.length; i++){
-      var denomValue = checkDenom(filteredCID[i][0]);
-      //console.log(filteredCID[i][0])
-      var cidValue = filteredCID[i][1]
-      //console.log(cidValue, count)
-      while (cidValue >= denomValue && count !== change) {
-        cidValue -= denomValue;
-        count += denomValue;
-        //console.log('Values', cidValue, count);
-        //changeArr.push([filteredCID[i][0], denomValue]);
-      }
-    }
-    return changeArr;
-  }
-  */
-  var count = 0;
   var changeArr = [];
+  // Função que calcula o troco
   function calculateChange(){
     //console.log('Change', change)
     //console.log('CID', filteredCID)
     for(var i = 0; i < filteredCID.length; i++) {
+      var count = 0;
       var denomValue = checkDenom(filteredCID[i][0]);
       var value = filteredCID[i][1];
       //console.log('denomValue', denomValue)
       //console.log(filteredCID[i])
+      console.log('Value', value, 'Change', change)
         while(value > 0 && change >= denomValue) {
-          //console.log('Change', change, 'Count', count, 'Value', value)
           //change -= denomValue;
           change -= denomValue;
           value -= denomValue;
           count += denomValue;
+
+          // Round change to the nearest hundreth deals with precision errors
+          change = Math.round(change * 100) / 100;
 
         }
       //console.log(count, value)
@@ -78,6 +67,7 @@ function checkCashRegister(price, cash, cid) {
       }
     }
     console.log(changeArr)
+    return changeArr;
   }
 
   var totalSum = 0;
@@ -94,10 +84,7 @@ function checkCashRegister(price, cash, cid) {
 
 }
 
-/*
 checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
-
 checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
-
-checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]])*/
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]])
 checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]])
